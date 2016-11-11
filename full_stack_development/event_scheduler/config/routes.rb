d29656d5fa4e9b4f693747e759adffc692b06102
sushmithaumashankar <sumashankar@qwinix.io>
2016-11-11 16:51:37 +0530

    full_stack_development

--- full_stack_development/event_scheduler/config/routes.rb
+++ full_stack_development/event_scheduler/config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   get "home_page" => "homepages#new", :as => "home_page"
   get "event_page" => "events#new", :as =>"event_page"
-  
-get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-root :to => "users#new"
-resources :users
-resources :sessions
+  get "event_new_form" => "events#newform", :as => "event_new_form"
+  post "event_new_form" => "events#create", :as => "event_new_create"
+  get "event_list" => "events#index", :as => "events_list"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
